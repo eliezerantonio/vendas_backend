61c554940e326d5c860284cb0c28b4be512fe99e
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-07-05 02:14:35 +0100

    costomizando o tratamento de rro da api

---
+++ src/shared/http/routes/index.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+const routes = Router();
+
+routes.get('/', (request, response) => {
+  return response.json({ message: 'Hello whord!' });
+});
+
+export default routes;
---
+++ src/shared/http/server.ts
@@ -0,0 +1,29 @@
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import routes from './routes';
+import AppError from '@shared/errors/AppError';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response
+        .status(error.statusCode)
+        .json({ status: 'error', message: error.message });
+    } else {
+      return response
+        .status(500)
+        .json({ status: 'error', messaage: 'Internal server error' });
+    }
+  },
+);
+
+app.listen(333, () => {
+  console.log('server startedt on port 3333');
+});
