bce69cd78e6960f2357b2c9a97f4f8d181dfe3da
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-10-13 00:41:14 +0100

    add typyorm config

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,9 @@
+import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
+import '@shared/typeorm';
 
 const app = express();
 
