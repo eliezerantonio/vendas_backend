058e5c8a6f86e20130e5b3525570d96141ec21bd
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-11-02 11:36:37 +0100

    Create ProductRepository

---
+++ src/modules/products/typeorm/repositories/ProductsRepository.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { EntityRepository, Repository } from 'typeorm';
+import Product from '../entities/Product';
+
+@EntityRepository(Product)
+export class ProductRepository extends Repository<Product> {
+  public async findByName(name: string): Promise<Product | undefined> {
+    const product = this.findOne({ where: { name } });
+
+    return product;
+  }
+}
