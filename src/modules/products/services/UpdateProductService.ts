7b0bd488a6982665b3bc88417c8d92c8759363c8
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-11-02 12:16:54 +0100

    rosolve logic problem on update service

--- src/modules/products/services/UpdateProductService.ts
+++ src/modules/products/services/UpdateProductService.ts
@@ -26,7 +26,7 @@ class UpdateProductService {
 
     const productExists = await productsRepository.findByName(name);
 
-    if (productExists) {
+    if (productExists && name !== product.name) {
       throw new AppError('There is already  one product with this name');
     }
 
