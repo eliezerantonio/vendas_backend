dfd49819d547c16023a49b3d022b19a30afb2ba8
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-11-02 21:05:25 +0100

    call products controller in routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
+import productsRouter from '@modules/products/routes/products.routes';
+
 const routes = Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello whord!' });
-});
+routes.use('/products', productsRouter);
 
 export default routes;
