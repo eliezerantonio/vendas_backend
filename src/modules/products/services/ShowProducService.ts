4adc7e6829464476e6c7635e8fdb0669d3e3d2b5
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-11-02 12:02:59 +0100

    Create ShowProductService

---
+++ src/modules/products/services/ShowProducService.ts
@@ -0,0 +1,23 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+
+interface IRequest {
+  id: string;
+}
+
+class ShowProductService {
+  public async execute({ id }: IRequest): Promise<Product | undefined> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const product = productsRepository.findOne(id);
+    if (!product) {
+      throw new AppError('Product not found.');
+    }
+
+    return product;
+  }
+}
+
+export default ShowProductService;
