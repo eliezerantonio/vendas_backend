7a47d345ed5b3d7783349a721ae6158d6359ec16
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-11-02 10:48:05 +0100

    create product entity

---
+++ src/modules/products/typeorm/entities/Product.ts
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  Entity,
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('products')
+class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column('decimal')
+  price: number;
+
+  @Column('int')
+  quantity: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Product;
