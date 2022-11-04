5d9a492df393f1f8e50ccc32a4e328886c25ab8d
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-11-04 18:30:23 +0100

    terminate validation in products routes

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -14,10 +14,35 @@ productsRouter.get(
   productsController.show,
 );
 
-productsRouter.post('/', productsController.create);
+productsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+  }),
+  productsController.create,
+);
 
-productsRouter.put('/:id', productsController.update);
+productsRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: { id: Joi.string().uuid().required() },
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+  }),
+  productsController.update,
+);
 
-productsRouter.delete('/:id', productsController.delete);
+productsRouter.delete(
+  '/:id',
+  celebrate({ [Segments.PARAMS]: { id: Joi.string().uuid().required() } }),
+  productsController.delete,
+);
 
 export default productsRouter;
