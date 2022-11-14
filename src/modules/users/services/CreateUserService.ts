e3e1d3feb196bce1c9d7d6c55607f0e0f7d167c3
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-11-14 11:54:49 +0100

    criptografando password before save user

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,5 +1,5 @@
 import AppError from '@shared/errors/AppError';
-
+import { hash } from 'bcryptjs';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UserRepository';
@@ -16,10 +16,16 @@ class CreateUserService {
     const emailExists = await usersRepository.findByEmail(email);
 
     if (emailExists) {
-      throw new AppError('email address already used');
+      throw new AppError('Email address already used');
     }
 
-    const user = usersRepository.create({ name, email, password });
+    const hashedPassword = await hash(password, 8);
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
 
     await usersRepository.save(user);
     return user;
