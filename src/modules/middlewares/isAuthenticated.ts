ba770d28f6a9996f863fb955ba06a1c5f26a3058
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-11-14 16:38:56 +0100

    create, add types for user

--- src/modules/middlewares/isAuthenticated.ts
+++ src/modules/middlewares/isAuthenticated.ts
@@ -3,6 +3,12 @@ import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 import authConfig from '@config/auth';
 
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
 export default function isAuthenticated(
   request: Request,
   response: Response,
@@ -17,7 +23,13 @@ export default function isAuthenticated(
   const [, token] = authHeader.split(' ');
 
   try {
-    const decodeToken = verify(token, authConfig.jwt.secret);
+    const decodedToken = verify(token, authConfig.jwt.secret);
+
+    const { sub } = decodedToken as TokenPayload;
+
+    request.user = {
+      id: sub,
+    };
 
     return next();
   } catch {
