81e21cf0427531ea9606a0ff510c09b03736a258
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-11-14 18:09:08 +0100

    config globals static routes to files

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -7,14 +7,17 @@ import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
+import uploadConfig from '@config/upload';
 import { errors } from 'celebrate';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 
 app.use(routes);
+
 app.use(errors());
 
 app.use(
