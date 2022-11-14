3dce3fe861b32571c4bfedbe01a374c11a139cfc
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-11-14 18:05:17 +0100

    upload file done

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import { celebrate, Joi, Segments } from 'celebrate';
+import multer from 'multer';
+import uploadConfig from '@config/upload';
+
 import UsersController from '../controllers/UsersController';
 import isAuthenticated from '@modules/middlewares/isAuthenticated';
+import UserAvatarController from '../controllers/UserAvatarController';
 
 const usersRouter = Router();
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
+const upload = multer(uploadConfig);
 
 usersRouter.get('/', isAuthenticated, usersController.index);
 
@@ -20,4 +26,10 @@ usersRouter.post(
   usersController.create,
 );
 
+usersRouter.patch(
+  '/avatar',
+  isAuthenticated,
+  upload.single('avatar'),
+  userAvatarController.update,
+);
 export default usersRouter;
