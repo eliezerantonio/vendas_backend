34dd6157ce025472810cd9b894653e54e4e45405
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-11-19 19:07:57 +0100

    create PasswordRoutes

---
+++ src/modules/users/routes/password.routes.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
+import ForgotPasswordController from '../controllers/ForgotPasswordController';
+
+const passwordRouter = Router();
+const forgotPasswordController = new ForgotPasswordController();
+
+passwordRouter.post(
+  '/forgot',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  forgotPasswordController.create,
+);
+
+export default passwordRouter;
