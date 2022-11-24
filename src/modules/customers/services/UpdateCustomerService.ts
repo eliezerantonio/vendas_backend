282ce557b5e4a3400746dfd48b7d195f34465f8e
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-11-24 08:38:38 +0100

    add UpdateCustomer

---
+++ src/modules/customers/services/UpdateCustomerService.ts
@@ -0,0 +1,36 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Customer from '../typeorm/entities/Customer';
+import CustomersRepository from '../typeorm/repositories/CustomersRepository';
+
+interface IRequest {
+  id: string;
+  name: string;
+  email: string;
+}
+
+class UpdateCustomerService {
+  public async execute({ id, name, email }: IRequest): Promise<Customer> {
+    const customersRepository = getCustomRepository(CustomersRepository);
+    const customer = await customersRepository.findById(id);
+
+    if (!customer) {
+      throw new AppError('Customer not found');
+    }
+
+    const customerExists = await customersRepository.findByEmail(email);
+
+    if (customerExists && email != customer.email) {
+      throw new AppError('There is aready one customer with this email');
+    }
+
+    customer.name = name;
+    customer.email = email;
+
+    await customersRepository.save(customer);
+
+    return customer;
+  }
+}
+
+export default UpdateCustomerService;
