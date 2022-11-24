b879669087a7c03d987bd4f7fa04ffb174842515
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-11-24 08:42:25 +0100

    add DeleteCustomerService

---
+++ src/modules/customers/services/DeleteCustomerService.ts
@@ -0,0 +1,23 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import CustomersRepository from '../typeorm/repositories/CustomersRepository';
+
+interface IRequest {
+  id: string;
+}
+
+class DeleteCusomerService {
+  public async execute({ id }: IRequest): Promise<void> {
+    const customersRepository = await getCustomRepository(CustomersRepository);
+
+    const customer = await customersRepository.findById(id);
+
+    if (!customer) {
+      throw new AppError(`Customer not found`);
+    }
+
+    await customersRepository.remove(customer);
+  }
+}
+
+export default DeleteCusomerService;
