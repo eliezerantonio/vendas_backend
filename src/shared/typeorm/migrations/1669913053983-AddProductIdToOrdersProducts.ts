a3273c6e14b6123002dacf5ab8dd3066dea12f02
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-12-01 17:55:54 +0100

    add foreignKey in Pivo Tables

---
+++ src/shared/typeorm/migrations/1669913053983-AddProductIdToOrdersProducts.ts
@@ -0,0 +1,36 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export class AddProductIdToOrdersProducts1669913053983
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'orders_product',
+      new TableColumn({ name: 'product_id', type: 'uuid', isNullable: true }),
+    );
+
+    await queryRunner.createForeignKey(
+      'orders_products',
+      new TableForeignKey({
+        name: 'OrdersProductsProduct',
+        columnNames: ['product_id'],
+        referencedTableName: 'products',
+        referencedColumnNames: ['id'],
+        onDelete: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey(
+      'orders_products',
+      'OrdersProductsProduct',
+    );
+    await queryRunner.dropColumn('orders_products', 'product_id');
+  }
+}
