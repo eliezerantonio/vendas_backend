f555f0c8b03e32e04a3920dba958f8212bf38a8a
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-12-05 01:38:15 +0100

    add OrdersRepository

---
+++ src/modules/orders/typeorm/repositories/OrdersRepository.ts
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import Customer from '@modules/customers/typeorm/entities/Customer';
+import { EntityRepository, Repository } from 'typeorm';
+import Order from '../entities/Order';
+interface IProduct {
+  product_id: string;
+  price: number;
+  quantity: number;
+}
+interface IRequest {
+  customer: Customer;
+  products: IProduct[];
+}
+
+@EntityRepository(Order)
+class OrdersRepository extends Repository<Order> {
+  public async findById(id: string): Promise<Order | undefined> {
+    const order = this.findOne(id, {
+      relations: ['order_products', 'customer'],
+    });
+
+    return order;
+  }
+
+  public async createOrder({ customer, products }: IRequest): Promise<Order> {
+    const order = this.create({ customer, order_products: products });
+
+    await this.save(order);
+
+    return order;
+  }
+}
+
+export default OrdersRepository;
