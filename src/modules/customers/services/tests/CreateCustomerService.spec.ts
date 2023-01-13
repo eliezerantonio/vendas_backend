ef3b7001c9d4ceba879277a90c8c344d1d8944eb
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-01-13 18:20:28 +0100

    Should not be able to create two customers with the same email

--- src/modules/customers/services/tests/CreateCustomerService.spec.ts
+++ src/modules/customers/services/tests/CreateCustomerService.spec.ts
@@ -1,5 +1,6 @@
 import FakeCustomersRepository from '@modules/customers/domain/repositories/fakes/FakeCustomersRepository';
 import CreateCustomerService from '../CreateCustomerService';
+import AppError from '@shared/errors/AppError';
 
 describe('CreateCustomer', () => {
   it('Should be able to create a new customer', async () => {
@@ -14,7 +15,21 @@ describe('CreateCustomer', () => {
     expect(customer).toHaveProperty('id');
   });
 
-  it('Should not be able to create two customers with the same email', () => {
-    expect(1).toBe(1);
+  it('Should not be able to create two customers with the same email', async () => {
+    const fakeCustomersRepository = new FakeCustomersRepository();
+
+    const createCustomer = new CreateCustomerService(fakeCustomersRepository);
+
+    await createCustomer.execute({
+      name: 'Eliezer Antonio',
+      email: 'eliezer@test.com',
+    });
+
+    expect(
+      createCustomer.execute({
+        name: 'Eliezer Antonio',
+        email: 'eliezer@test.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
